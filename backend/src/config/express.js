e9f77d3475668cbe213db5a9b1e7a931e7d6e56b
danrley <danrley.teixeira@hp.com>
2019-01-23 18:59:42 -0300

    Creating express configuration

---
+++ backend/src/config/express.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('../api/routes/user');
+const uploadRoutes = require('../api/routes/upload');
+
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+module.exports = function() {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(allowCrossDomain);
+  userRoutes(app);
+  uploadRoutes(app);
+  return app;
+}
