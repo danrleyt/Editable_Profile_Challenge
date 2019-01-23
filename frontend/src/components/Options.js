8153d8950c0b74109b9748da8b9f8e9f020e6b4b
danrley <danrley.teixeira@hp.com>
2019-01-22 21:11:08 -0300

    adding select component for single selection components

---
+++ frontend/src/components/Options.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Input } from 'react-materialize';
+
+const Options = (props) => {  
+  let options = props.data.map((option, index) => {
+    return (
+      <option value={index} key={option.id}>{option.name}</option>
+    )
+  });
+  
+  if(!props.required) {
+    let line = <option value={null}></option>
+    options = [line, options]
+  }
+
+  return (
+    <Input s={3} type='select' label={props.fieldLabel} name={props.fieldName} onChange={props.onChange}>
+      {options}
+    </Input>
+  )
+}
+
+export default Options;
