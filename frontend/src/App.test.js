3b69e3d48afe247f4f2a49d8e4cf44e4b49a65a9
Danrley <danrleyt@gmail.com>
2019-01-28 15:11:07 -0300

    adding first tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import faker from 'faker';
+import puppeteer from 'puppeteer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+describe('Signup Page', () => {
+  test('Signup button shows up', async () => {
+    let browser = await puppeteer.launch({
+      headless: false
+    });
+    let page = await browser.newPage();
+    page.emulate({
+      viewport: {
+        width: 500,
+        heigh: 2400
+      },
+      userAgent: ''
+    });
+
+    await page.goto('http://localhost:3000');
+
+  })
+})
